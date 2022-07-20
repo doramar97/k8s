114794cdb09b3bbd03ccb7353eca7d36d623d118
“doramar97” <doramar5555@gmail.com>
2022-07-20 03:59:46 -0700

    Added StatefulSet With PVC

--- CKA-practice-exercises/storage/dor.sh
+++ CKA-practice-exercises/storage/dor.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 kubectl apply -f volume.yaml && kubectl apply -f pvc-cka.yaml && kubectl apply -f pod.yaml
+
+kubectl exec -it cka-pod bash 
+
+cd usr/share/nginx/html 
+ 
+cat dor.html
